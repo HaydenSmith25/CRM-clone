3f06dd6daa47a89f47b8561e1cdd90b056445a95
Hayden Smith <hayden.smith-25@outlook.com>
2022-04-04 16:06:44 -0500

    handled functionality and styling for progress component

--- src/components/ProgressDisplay.js
+++ src/components/ProgressDisplay.js
@@ -1,5 +1,14 @@
-function ProgressDisplay() {
-  return <div>ProgressDisplay</div>;
+function ProgressDisplay({ progress }) {
+  return (
+    <div className="progress-dsiplay">
+      <div className="progress-bar">
+        <div
+          style={{ width: progress + "%" }}
+          className="progress-indicator"
+        ></div>
+      </div>
+    </div>
+  );
 }
 
 export default ProgressDisplay;
