5db1167a59232302f4828c4fa4b54f0db3dae8bd
Hayden Smith <hayden.smith-25@outlook.com>
2022-04-04 16:07:25 -0500

    added functionality and styling for status component

--- src/components/StatusDisplay.js
+++ src/components/StatusDisplay.js
@@ -1,5 +1,30 @@
-function StatusDisplay() {
-  return <div>StatusDisplay</div>;
+function StatusDisplay({ status }) {
+  const getColor = (status) => {
+    let color;
+    switch (status) {
+      case "done":
+        color = "green";
+        break;
+      case "working on it":
+        color = "yellow";
+        break;
+      case "stuck":
+        color = "red";
+        break;
+      default:
+        color = "gray";
+    }
+    return color;
+  };
+
+  return (
+    <div
+      className="status-dsiplay"
+      style={{ backgroundColor: getColor(status) }}
+    >
+      {status}
+    </div>
+  );
 }
 
 export default StatusDisplay;
